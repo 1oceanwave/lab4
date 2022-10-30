8fd68cdbcaf2aec94e510fd5e2f2930ab8ec2d79
1oceanwave <hesoyama_xd@mail.ru>
2022-10-30 20:32:55 +0300

    create login

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
-import requests
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import psycopg2
 app = Flask(__name__)
-conn = psycopg2.connect(database="service_db",
+conn = psycopg2.connect(database="auth_db",
                         user="postgres",
-                        password="ffqeoogl35Ll",
+                        password="92657792Sha_",
                         host="localhost",
                         port="5432")
 cursor = conn.cursor()
-
+# if "".join(username.split()) != "" and "".join(password.split()) != "":
+#     error = ""
+# else:
+#     error = "не вверный логин или пароль"
 @app.route('/login/', methods=['POST', 'GET'])
 def login():
+    error = ''
+    username = request.form.get('username')
+    password = request.form.get('password')
     if request.method == 'POST':
-        if request.form.get("login"):
-            username = request.form.get('username')
-            password = request.form.get('password')
-            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-            records = list(cursor.fetchall())
+        cursor.execute("SELECT * FROM auth.users WHERE login=%s AND password=%s", (str(username), str(password)))
+        records = list(cursor.fetchall())
+        return render_template('account.html', full_name=records[0][1], username=records[0][2], password=records[0][3])
+    if username == "" or password == "":
+        error = "не вверный логин или пароль"
+    else:
+        error = ""
 
-            return render_template('account.html', full_name=records[0][1])
-        elif request.form.get("registration"):
-            return redirect("/registration/")
-
-    return render_template('login.html')
-
-@app.route('/registration/', methods=['POST', 'GET'])
-def registration():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        login = request.form.get('login')
-        password = request.form.get('password')
-
-        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
-                       (str(name), str(login), str(password)))
-        conn.commit()
-
-        return redirect('/login/')
-
-    return render_template('registration.html')
+    return render_template('login.html', error = error)
